6354715ad2154bf66c071191ec869840a78da8f7
Marisol López <jennymlo2399@gmail.com>
2025-05-29 22:44:31 -0600

    feat: update structure

--- electron/main.js
+++ electron/main.js
@@ -1,20 +1,36 @@
-import { app, BrowserWindow } from 'electron'
-import { join } from 'path'
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..');
+
+function createWindow() {
+  const window = new BrowserWindow({
+    title: 'Pixel Puppy',
+    width: 1280,
+    height: 832,
+    titleBarStyle: 'hidden',
+    frame: false,
     webPreferences: {
       preload: join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
     }
-  })
+  });
 
-  win.loadURL('http://localhost:5173') // Vite dev server
+  const isDev = process.env.NODE_ENV === 'development';
+  const startUrl = isDev 
+    ? 'http://localhost:5173' // In dev mode load vite
+    : `file://${join(__dirname, '../dist/index.html')}`; // In production, load the compiled file
+
+  window.loadURL(startUrl);
+  if (isDev) window.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
