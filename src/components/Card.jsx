d48fc610ee4334db28cedf7d39ec04f9366dd62c
Marisol López <jennymlo2399@gmail.com>
2025-05-29 18:27:35 -0600

    feat: add background

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card () {
+    return (
+        <div className="card">
+            <div className="card-content">
+                <p>what should I wear today?</p>
+            </div>
+        </div>
+    )
+}
