f981fafb67b1e910f40d494f510ae2406ef04dfc
Marisol López <jennymlo2399@gmail.com>
2025-08-07 21:17:18 -0600

    feat: add options and choose color and clothes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,19 @@
+import Options from './Options';
+import ChooseClothes from './ChooseClothes';
+import ChooseColor from './ChooseColor';
+
 export default function Card () {
     return (
         <div className="card">
             <div className="card-content">
                 <p>what should I wear today?</p>
+                <div className="card-background"/>
+
+                <ChooseColor/>
+
+                <Options/>
+                
+                <ChooseClothes/>
             </div>
         </div>
     )
