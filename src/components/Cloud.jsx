7e74729415ac211ec9cd3ac9d7303d2d5bec01d8
Marisol López <jennymlo2399@gmail.com>
2025-08-11 22:28:50 -0600

    style: update images of background

--- src/components/Cloud.jsx
+++ src/components/Cloud.jsx
@@ -1,10 +1,10 @@
-import image1 from '../assets/cloud-1.png';
-import image2 from '../assets/cloud-2.png';
-import image3 from '../assets/cloud-3.png';
-import image4 from '../assets/cloud-4.png';
-import image5 from '../assets/cloud-5.png';
-import image6 from '../assets/cloud-6.png';
-import image7 from '../assets/cloud-7.png';
+import image1 from '../assets/background/cloud-1.png';
+import image2 from '../assets/background/cloud-2.png';
+import image3 from '../assets/background/cloud-3.png';
+import image4 from '../assets/background/cloud-4.png';
+import image5 from '../assets/background/cloud-5.png';
+import image6 from '../assets/background/cloud-6.png';
+import image7 from '../assets/background/cloud-7.png';
 
 export default function Cloud ({ number }) {
 
