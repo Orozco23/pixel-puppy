843054a9a6c0256cd3c21bfa7f416c2e456379fe
Marisol López <jennymlo2399@gmail.com>
2025-08-13 13:32:49 -0600

    refactor: change div to button

--- src/components/ChooseColor.jsx
+++ src/components/ChooseColor.jsx
@@ -3,16 +3,16 @@ import arrow from '../assets/background/arrow.png';
 export default function ChooseColor ({ onPrev, onNext }) {
     return (
         <>
-            <div className='left-arrow'>
+            <button className='left-arrow'>
                 <img src={arrow} alt="Left Arrow" 
                     onClick={onPrev}
                 />
-            </div>
-            <div className='right-arrow'>
+            </button>
+            <button className='right-arrow'>
                 <img src={arrow} alt="Right Arrow" 
                     onClick={onNext}
                 />
-            </div>
+            </button>
         </>
     )
 }
