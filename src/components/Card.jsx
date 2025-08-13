88b8f7624c6963e8bdde6d52c6e5c797d73bc310
Marisol López <jennymlo2399@gmail.com>
2025-08-12 19:42:52 -0600

    feat: add outfit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
 import Options from './Options';
 import ChooseClothes from './ChooseClothes';
 import ChooseColor from './ChooseColor';
+import Outfit from './Outfit';
 
 export default function Card () {
     return (
         <div className="card">
             <div className="card-content">
                 <p>what should I wear today?</p>
-                <div className="card-background"/>
+                
+                <div className="card-background">
+                    <Outfit/>
+                </div>
 
                 <ChooseColor/>
 
